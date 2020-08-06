e9473322805377054da75be7b35e92cf549313c2
Felipe Martins <felipemjesus@outlook.com>
2020-08-05 22:50:58 -0300

    Criado recurso de recuperar total de conexões

--- src/controllers/ConnectionsController.ts
+++ src/controllers/ConnectionsController.ts
@@ -3,6 +3,14 @@ import db from '../database/connection';
 
 export default class ConnectionsController {
 
+  async index(request: Request, response: Response) {
+    const totalConnections = await db('connections').count('* as total');
+
+    const { total } = totalConnections[0];
+
+    response.json({ total });
+  }
+
   async create(request: Request, response: Response) { 
     const { user_id } = request.body;
 
